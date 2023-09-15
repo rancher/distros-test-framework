99e365a56cab8544946c29e75487cb673c9bdb0f
Francisco <francisco.moral@suse.com>
2023-09-15 08:31:12 -0300

    feat: coredns bump
    Signed-off-by: Francisco <francisco.moral@suse.com>

---
+++ entrypoint/versionbump/coredns.go
@@ -0,0 +1,73 @@
+//go:build coredns
+
+package versionbump
+
+import (
+	"fmt"
+
+	"github.com/rancher/distros-test-framework/pkg/customflag"
+	"github.com/rancher/distros-test-framework/pkg/template"
+	"github.com/rancher/distros-test-framework/pkg/testcase"
+	"github.com/rancher/distros-test-framework/shared"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("VersionTemplate Upgrade:", func() {
+	It("Start Up with no issues", func() {
+		testcase.TestBuildCluster(GinkgoT())
+	})
+
+	// It("Validate Nodes", func() {
+	// 	testcase.TestNodeStatus(
+	// 		assert.NodeAssertReadyStatus(),
+	// 		nil)
+	// })
+	//
+	// It("Validate Pods", func() {
+	// 	testcase.TestPodStatus(
+	// 		assert.PodAssertRestart(),
+	// 		assert.PodAssertReady(),
+	// 		assert.PodAssertStatus())
+	// })
+
+	It("Install helm and validate traefik repo", func() {
+		_, err := shared.ManageWorkload(
+			"create",
+			"dnsutils.yaml",
+		)
+		Expect(err).NotTo(HaveOccurred(), "dnsutils manifest not deployed")
+		_, err = shared.AddHelmRepo("traefik", "https://helm.traefik.io/traefik")
+		Expect(err).NotTo(HaveOccurred(), "Traefik repo not added")
+	})
+
+	It("Verifies bump version on rke2 for canal with calico and flannel versions", func() {
+		template.VersionTemplate(template.VersionTestTemplate{
+			TestCombination: &template.RunCmd{
+				Run: []template.TestMap{
+					{
+						Cmd:                  "helm list --all-namespaces,kubectl exec -n dnsutils -t dnsutils : -- nslookup kubernetes.default",
+						ExpectedValue:        template.TestMapTemplate.ExpectedValue,
+						ExpectedValueUpgrade: template.TestMapTemplate.ExpectedValueUpgrade,
+					},
+				},
+			},
+			InstallMode: customflag.ServiceFlag.InstallMode.String(),
+			TestConfig: &template.TestConfig{
+				TestFunc:       template.ConvertToTestCase(customflag.ServiceFlag.TestConfig.TestFuncs),
+				DeployWorkload: customflag.ServiceFlag.TestConfig.DeployWorkload,
+				WorkloadName:   customflag.ServiceFlag.TestConfig.WorkloadName,
+			},
+			Description: customflag.ServiceFlag.TestConfig.Description,
+		})
+	})
+})
+
+var _ = AfterEach(func() {
+	if CurrentSpecReport().Failed() {
+		fmt.Printf("\nFAILED! %s\n", CurrentSpecReport().FullText())
+	} else {
+		fmt.Printf("\nPASSED! %s\n", CurrentSpecReport().FullText())
+	}
+})
