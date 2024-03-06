fe3c16a1103806c6040cba2061d14b305d735322
fmoral2 <francisco.moral@suse.com>
2024-03-06 08:45:00 -0300

    Snapshot webhook rke2 test (#75)
    snapshot webhook rke2 test

---
+++ pkg/testcase/snapshotwebhook.go
@@ -0,0 +1,28 @@
+package testcase
+
+import (
+	"strings"
+
+	"github.com/rancher/distros-test-framework/shared"
+)
+
+func TestSnapshotWebhook(applyWorkload bool) error {
+	assert := " Spec.VolumeSnapshotClassName must not be the empty string"
+
+	if applyWorkload {
+		workloadErr := shared.ManageWorkload("apply", "snapshot-webhook.yaml")
+		if workloadErr != nil {
+			if strings.Contains(workloadErr.Error(), assert) {
+				shared.LogLevel("error", workloadErr.Error())
+				shared.LogLevel("info", "Snapshot Webhook manifest not deployed, "+
+					"as expected related to empty string")
+				return workloadErr
+			}
+			shared.LogLevel("error", workloadErr.Error(),
+				"Error: webhook not rejecting bad VolumeSnapshot, please double check")
+			return nil
+		}
+	}
+
+	return nil
+}
