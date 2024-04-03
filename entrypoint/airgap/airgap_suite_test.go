4f5fbd98f2dbb6c9458f10994fe1bae4f274f3bf
mdrahman-suse <mdrahman@suse.com>
2024-04-03 10:27:16 -0500

    Initial commit for airgap setup

---
+++ entrypoint/airgap/airgap_suite_test.go
@@ -0,0 +1,41 @@
+package airgap
+
+import (
+	"flag"
+	"os"
+	"testing"
+
+	"github.com/rancher/distros-test-framework/factory"
+	"github.com/rancher/distros-test-framework/pkg/customflag"
+	"github.com/rancher/distros-test-framework/shared"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestMain(m *testing.M) {
+	var err error
+	flag.Var(&customflag.ServiceFlag.ClusterConfig.Destroy, "destroy", "Destroy cluster after test")
+	flag.Parse()
+
+	_, err = shared.EnvConfig()
+	if err != nil {
+		return
+	}
+
+	os.Exit(m.Run())
+}
+
+func TestAirgapSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Create Airgap Cluster Test Suite")
+}
+
+var _ = AfterSuite(func() {
+	g := GinkgoT()
+	if customflag.ServiceFlag.ClusterConfig.Destroy {
+		status, err := factory.DestroyCluster(g)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(status).To(Equal("cluster destroyed"))
+	}
+})
