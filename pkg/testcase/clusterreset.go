23c55ccc9e63a059ad49efb58a29d850023cfd8c
ERYN Dawkins <eryn.dawkins@suse.com>
2024-05-07 17:52:33 -0500

    sleep and updated logging and CI fixes

--- pkg/testcase/clusterreset.go
+++ pkg/testcase/clusterreset.go
@@ -2,6 +2,7 @@ package testcase
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/rancher/distros-test-framework/factory"
 	"github.com/rancher/distros-test-framework/shared"
@@ -39,25 +40,17 @@ func TestClusterReset() {
 		Expect(resetCmdErr.Error()).To(ContainSubstring("Managed etcd cluster"))
 		Expect(resetCmdErr.Error()).To(ContainSubstring("has been reset"))
 	}
-<<<<<<< Updated upstream
-	shared.LogLevel("info", "cluster reset successful")
-=======
 	shared.LogLevel("info", "cluster reset successful. Waiting 60 seconds for cluster "+
 		"to complete background processes after reset.")
 	time.Sleep(60 * time.Second)
->>>>>>> Stashed changes
 
 	deleteDataDirectories(cluster)
 	shared.LogLevel("info", "data directories deleted")
 	startServer(cluster)
-<<<<<<< Updated upstream
-	shared.LogLevel("info", "%s-service started", cluster.Config.Product)
-=======
 	shared.LogLevel("info", "%s-service started. Waiting 60 seconds for nodes "+
 		"and pods to sync after reset.", cluster.Config.Product)
 
 	time.Sleep(60 * time.Second)
->>>>>>> Stashed changes
 }
 
 func killall(cluster *factory.Cluster) {
