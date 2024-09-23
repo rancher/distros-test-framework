b98429b6918dffc822d20557c397716142fbd9be
ERYN Dawkins <eryn.dawkins@suse.com>
2024-09-23 18:05:51 -0500

    adding function to create new Instance

--- pkg/testcase/clusterresetrestore.go
+++ pkg/testcase/clusterresetrestore.go
@@ -2,15 +2,20 @@ package testcase
 
 import (
 	"fmt"
-
-	"github.com/rancher/distros-test-framework/shared"
-
 	"os"
+	"strings"
+
+	"github.com/rancher/distros-test-framework/pkg/aws"
+	"github.com/rancher/distros-test-framework/shared"
 
 	. "github.com/onsi/gomega"
 )
 
-func TestClusterResetRestoreS3Snapshot(cluster *shared.Cluster, applyWorkload, deleteWorkload bool) {
+func TestClusterResetRestoreS3Snapshot(
+	cluster *shared.Cluster,
+	applyWorkload,
+	deleteWorkload bool,
+) {
 	var workloadErr error
 	if applyWorkload {
 		workloadErr = shared.ManageWorkload("apply", cluster.Config.Product+"-extra-metadata.yaml")
@@ -25,7 +30,16 @@ func TestClusterResetRestoreS3Snapshot(cluster *shared.Cluster, applyWorkload, d
 	secretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
 	s3Region := cluster.AwsEc2.Region
 
-	takeS3Snapshot(cluster, s3Bucket, s3Folder, s3Region, accessKeyID, secretAccessKey, true, false)
+	takeS3Snapshot(
+		cluster,
+		s3Bucket,
+		s3Folder,
+		s3Region,
+		accessKeyID,
+		secretAccessKey,
+		true,
+		false,
+	)
 
 	onDemandPathCmd := fmt.Sprintf("sudo ls /var/lib/rancher/%s/server/db/snapshots", cluster.Config.Product)
 	onDemandPath, _ := shared.RunCommandOnNode(onDemandPathCmd, cluster.ServerIPs[0])
@@ -37,17 +51,35 @@ func TestClusterResetRestoreS3Snapshot(cluster *shared.Cluster, applyWorkload, d
 
 	fmt.Println("\ntoken: ", clusterToken)
 
-	// stopInstances()
+	stopInstances()
 	// create fresh new VM and install K3s/RKE2 using RunCommandOnNode
+	createNewServer(cluster)
 	// how do I delete the instances, bring up a new instance and install K3s/RKE2 using what we currently have?
 	shared.LogLevel("info", "running cluster reset on server %s\n", cluster.ServerIPs[0])
-	restoreS3Snapshot(cluster, s3Bucket, s3Folder, s3Region, onDemandPath, accessKeyID, secretAccessKey, clusterToken)
+	restoreS3Snapshot(
+		cluster,
+		s3Bucket,
+		s3Folder,
+		s3Region,
+		onDemandPath,
+		accessKeyID,
+		secretAccessKey,
+		clusterToken,
+	)
 
 }
 
 // perform snapshot and list snapshot commands -- deploy workloads after snapshot [apply workload]
-func takeS3Snapshot(cluster *shared.Cluster, s3Bucket, s3Folder, s3Region, accessKeyID, secretAccessKey string,
-	applyWorkload, deleteWorkload bool) {
+func takeS3Snapshot(
+	cluster *shared.Cluster,
+	s3Bucket,
+	s3Folder,
+	s3Region,
+	accessKeyID,
+	secretAccessKey string,
+	applyWorkload,
+	deleteWorkload bool,
+) {
 	productLocationCmd, findErr := shared.FindPath(cluster.Config.Product, cluster.ServerIPs[0])
 	Expect(findErr).NotTo(HaveOccurred())
 
@@ -69,25 +101,62 @@ func takeS3Snapshot(cluster *shared.Cluster, s3Bucket, s3Folder, s3Region, acces
 
 }
 
-func restoreS3Snapshot(cluster *shared.Cluster, s3Bucket, s3Folder, s3Region, onDemandPath, accessKeyID, secretAccessKey, token string) {
+func restoreS3Snapshot(
+	cluster *shared.Cluster,
+	s3Bucket,
+	s3Folder,
+	s3Region,
+	onDemandPath,
+	accessKeyID,
+	secretAccessKey,
+	token string,
+) {
 	// var path string
 	productLocationCmd, findErr := shared.FindPath(cluster.Config.Product, cluster.ServerIPs[0])
 	Expect(findErr).NotTo(HaveOccurred())
 	resetCmd := fmt.Sprintf("sudo %s server --cluster-reset --etcd-s3 --cluster-reset-restore-path=%s"+
-		"--etcd-s3-bucket=%s --etcd-s3-folder=%s --etcd-s3-region=us-east-2 --etcd-s3-access-key=%s"+
-		"--etcd-s3-secret-key=%s --token=%s", productLocationCmd, s3Bucket, s3Folder, s3Region, accessKeyID,
-		secretAccessKey)
+		"--etcd-s3-bucket=%s --etcd-s3-folder=%s --etcd-s3-region=%s --etcd-s3-access-key=%s"+
+		"--etcd-s3-secret-key=%s --token=%s", productLocationCmd, onDemandPath, s3Bucket, s3Folder, s3Region, accessKeyID,
+		secretAccessKey, token)
 	resetRes, resetCmdErr := shared.RunCommandOnNode(resetCmd, cluster.ServerIPs[0])
 	Expect(resetCmdErr).NotTo(HaveOccurred())
 	Expect(resetRes).To(ContainSubstring("Managed etcd cluster"))
 	Expect(resetRes).To(ContainSubstring("has been reset"))
 }
 
+func stopInstances()
+
+func createNewServer(cluster *shared.Cluster) {
+	
+	resourceName := os.Getenv("resource_name")
+	awsDependencies, err := aws.AddAWSClient(cluster)
+	Expect(err).NotTo(HaveOccurred(), "error adding aws nodes: %s", err)
+
+	// create server names.
+	var (
+		serverName,
+		instanceServerId,
+		newExternalServerIp,
+		newPrivateServerIp,
+	)
+		
+	
+	serverName = append(serverName, fmt.Sprintf("%s-server-fresh", resourceName))	
+
+	var createErr error
+	newExternalServerIp, newPrivateServerIp, instanceServerId, createErr =
+		awsDependencies.CreateInstances(serverName...)
+	Expect(createErr).NotTo(HaveOccurred(), createErr)
+}
+
 // make sure the workload you deployed after the snapshot isn't present after the restore snapshot
 
-// func installProduct() {
-
-// }
+func installProduct(cluster *share.cluster) {
+	version := cluster.Config.Version
+	if cluster.Config.Product == "k3s" {
+		installCmd := fmt.Sprint("curl -sfL https://get.k3s.io/ | sudo INSTALL_K3S_VERSION=%s INSTALL_K3S_SKIP_ENABLE=true sh -")
+	}
+}
 
 // func deleteOldNodes() {
 
