b44fb9dc5042cc60bc04cbc1e4a280689bde5312
Francisco <francisco.moral@suse.com>
2024-10-23 19:33:01 -0300

    add qase run workflow 4
    Signed-off-by: Francisco <francisco.moral@suse.com>

--- docs/qase.md
+++ docs/qase.md
@@ -1,2 +1,23 @@
 ## [Qase-Reporter](../pkg/qase/README.md)
+## [Qase-Patch-Validation-Create](#qase-patch-validation-create)
 
+
+# Qase-Patch-Validation-Create
+
+### Description
+This job aims to automatically create a patch validation in Qase for a given project and test plan ID.
+
+### Required Parameters
+All variables are already defined or set in run time.
+- `QASE_API_TOKEN`: Qase API token to authenticate with the Qase API.               # Set as secret on GH actions
+- `QASE_PROJECT_CODE`: Qase project code to create the patch validation for.        # Set as variable inside yaml.
+- `QASE_TEST_PLAN_ID`: Qase test plan ID to create the patch validation for.        # Set as variable inside yaml.
+- `QASE_TAG`: Tag to filter the team the patch validation.                          # Set as variable inside yaml.
+- `QASE_MILESTONE`: Milestone from the given patch.                                 # Set as variable inside yaml.
+
+
+### Usage
+- Go to gh actions https://github.com/rancher/distros-test-framework/actions
+- Click on `run workflow` and select the `Qase-Patch-Validation-Create` workflow.
+- Fill in the required parameters which are versions and rcs target.
+- The job based on parameters above will create the title,description and milestone for the run.
---
+++ scripts/qase-patch-validation.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+
+# Qase Patch Validation Run Creation Script.
+
+PS4='+(${LINENO}): '
+set -e
+trap 'echo "Error on line $LINENO: $BASH_COMMAND"' ERR
+
+CREATE_MILESTONE=${1}
+QASE_MILESTONE=${2}
+
+validate_params() {
+  validate_token
+    if [[ -z "$QASE_PROJECT_CODE" ]]; then
+        echo "Error: Missing required var QASE_PROJECT_CODE."
+        exit 1
+    fi
+
+    if [[ -z "$QASE_TEST_PLAN_ID" ]]; then
+        echo "Error: Missing required var QASE_TEST_PLAN_ID."
+        exit 1
+    fi
+
+    if [[ -z "$QASE_TAG" ]]; then
+        echo "Error: Missing required var QASE_TA."
+        exit 1
+    fi
+}
+
+validate_token() {
+    if [[ -z "$QASE_API_TOKEN" ]]; then
+        echo "Error: Missing required var QASE_API_TOKEN."
+        exit 1
+    fi
+}
+
+create_milestone() {
+    if [[ -z "$QASE_MILESTONE" ]]; then
+        echo "Error: Missing required QASE_MILESTONE."
+        exit 1
+    fi
+
+     RESPONSE=$(curl --request POST \
+        --url "https://api.qase.io/v1/milestone/$QASE_PROJECT_CODE" \
+        --header "Token: $QASE_API_TOKEN" \
+        --header 'Content-Type: application/json' \
+        --data '{
+                "title": "'"$QASE_MILESTONE"'"
+            }')
+
+    # extract milestone ID from response.
+    MILESTONE_ID=$(echo "$RESPONSE" | jq '.result.id')
+    if [[ -z "$MILESTONE_ID" || "$MILESTONE_ID" == "null" ]]; then
+        echo "Failed to create milestone."
+        exit 1
+    fi
+
+    echo "$MILESTONE_ID"
+}
+
+# Function to create test run with given parameters being: title, description, milestone id,plan id and tag.
+create_test_run() {
+     TAG_JSON='["'"$QASE_TAG"'"]'
+
+    RESPONSE=$(curl --request POST \
+        --url "https://api.qase.io/v1/run/$QASE_PROJECT_CODE" \
+        --header "Token: $QASE_API_TOKEN" \
+        --header 'Content-Type: application/json' \
+        --data '{
+                "title": "'"$TITLE"'",
+                "description": "'"$DESCRIPTION"'",
+                "milestone_id": '"$MILESTONE_ID"',
+                "tags": '"$TAG_JSON"',
+                "include_all_cases": false,
+                "plan_id": '"$QASE_TEST_PLAN_ID"'
+            }')
+
+    echo "response status is: $RESPONSE"
+
+    # extract run ID from response, fail if not found since its crutial step.
+    RUN_ID=$(echo "$RESPONSE" | jq '.result.id')
+    if [[ -z "$RUN_ID" || "$RUN_ID" == "null" ]]; then
+        echo "Failed to create test run."
+        exit 1
+    fi
+
+    echo "Created test run with ID: $RUN_ID"
+}
+
+main() {
+    # If we provide milestone == true, create milestone and return.
+    if [[ "$CREATE_MILESTONE" == "true" ]]; then
+        validate_token
+        create_milestone
+        exit 0
+    else
+      validate_params
+      create_test_run
+    fi
+}
+
+main "$@"
