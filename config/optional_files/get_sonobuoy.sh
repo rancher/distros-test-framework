8b1ef312c202cf2621e7e0668cc0997669409458
VestigeJ <justin.janes@suse.com>
2024-12-20 11:14:42 -0800

    removing shell script artifact get_sonobuoy.sh

--- config/optional_files/get_sonobuoy.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-get_sono() {
-    has_bin wget
-    _arch
-    arch=$(if [ "$(uname -m)" = "x86_64" ]; then echo "amd64"; else echo "arm64"; fi)
-    wget https://github.com/vmware-tanzu/sonobuoy/releases/download/v0.57.1/sonobuoy_0.57.1_linux_"${arch}".tar.gz
-    sudo tar -xzf sonobuoy_0.57.1_linux_"${arch}".tar.gz -C /usr/local/bin
-}
