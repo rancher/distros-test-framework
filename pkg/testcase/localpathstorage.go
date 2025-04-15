b8ebc59f5b018ac9d2b7dfd05f5663fd3cdd784a
Archana Ganesh <archana.ganesh@suse.com>
2025-04-15 12:53:08 -0700

    adding more logging

--- pkg/testcase/localpathstorage.go
+++ pkg/testcase/localpathstorage.go
@@ -1,6 +1,8 @@
 package testcase
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/rancher/distros-test-framework/pkg/assert"
@@ -79,13 +81,17 @@ func readData(cluster *shared.Cluster) error {
 }
 
 func createDir(cluster *shared.Cluster) {
-	shared.LogLevel("debug", "Node OS: %s ", cluster.NodeOS)
+	shared.LogLevel("debug", "node OS: %s ", cluster.NodeOS)
 	if cluster.NodeOS == "slemicro" {
 		var output string
 		var mkdirErr error
+		dirpath := fmt.Sprintf("/var/lib/rancher/%s/storage", cluster.Config.Product)
 		cmd := "test -d '/opt/data' && echo 'directory exists: /opt/data' || sudo mkdir -p /opt/data; ls -lrt /opt"
-		shared.LogLevel("debug", "Create /opt/data directory with cmd: %s", cmd)
+		cmd2 := fmt.Sprintf(
+			"test -d '%s' && echo 'directory exists: %s' || sudo mkdir -p %s; sudo ls -lrtR %s",
+			dirpath, dirpath, dirpath, dirpath)
 		for _, ip := range append(cluster.ServerIPs, cluster.AgentIPs...) {
+			shared.LogLevel("debug", "create /opt/data directory with cmd: %s", cmd)
 			output, mkdirErr = shared.RunCommandOnNode(cmd, ip)
 			if mkdirErr != nil {
 				shared.LogLevel("warn", "error creating /opt/data dir on node ip: %s", ip)
@@ -93,18 +99,27 @@ func createDir(cluster *shared.Cluster) {
 			if output != "" {
 				shared.LogLevel("debug", "create and check /opt/data output: %s", output)
 			}
+			shared.LogLevel("debug", "Create %s directory with cmd: %s", dirpath, cmd)
+			output, mkdirErr = shared.RunCommandOnNode(cmd2, ip)
+			if mkdirErr != nil {
+				shared.LogLevel("warn", "error creating %s dir on node ip: %s", dirpath, ip)
+			}
+			if output != "" {
+				shared.LogLevel("debug", "create and check %s output: %s", dirpath, output)
+			}
 		}
 	}
 }
 
 func logPodData(cluster *shared.Cluster) {
-	shared.LogLevel("debug", "Logging pod logs and describe pod output")
+	shared.LogLevel("debug", "logging pod logs and describe pod output for pods with namespace: %s", namespace)
+	// Pod Log with namespace: local-path-storage
 	filters := map[string]string{
 		"namespace": namespace,
 	}
 	pods, getErr := shared.GetPodsFiltered(filters)
 	if getErr != nil {
-		shared.LogLevel("error", "Possibly no pods found with namespace: %s", namespace)
+		shared.LogLevel("error", "possibly no pods found with namespace: %s", namespace)
 	}
 	for i := range pods {
 		if pods[i].NameSpace == "" {
@@ -112,5 +127,32 @@ func logPodData(cluster *shared.Cluster) {
 		}
 		shared.LoggerPodLogs(cluster, &pods[i])
 		shared.DescribePod(cluster, &pods[i])
+	}
+	// Pod log for helper-pod-create-pvc
+	shared.LogLevel("debug", "logging pod logs and describe pod output for pod with name: helper-pod-create-pvc*")
+	filters = map[string]string{
+		"namespace": "kube-system",
+	}
+
+	ksPods, getPodErr := shared.GetPodsFiltered(filters)
+	if getPodErr != nil {
+		shared.LogLevel("error", "error getting pods with namespace: kube-system")
+	}
+	for i := range ksPods {
+		if strings.Contains(ksPods[i].Name, "helper-pod-create-pvc") {
+			if ksPods[i].NameSpace == "" {
+				ksPods[i].NameSpace = "kube-system"
+			}
+			shared.LoggerPodLogs(cluster, &ksPods[i])
+			shared.DescribePod(cluster, &ksPods[i])
+		}
+	}
+	// Log the kubectl get pv,pvc,storageclass
+	output, getErr := shared.KubectlCommand(cluster, "node", "get", "pv,pvc,storageclass", "-A")
+	if getErr != nil {
+		shared.LogLevel("error", "error getting pv,pvc and storageclass info")
+	}
+	if output != "" {
+		shared.LogLevel("debug", "pv,pvc,storageclass info:\n %s", output)
 	}
 }
