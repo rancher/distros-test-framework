f025d73fbee2077c5ebd60cdd30ec7ea5a259ea8
Archana Ganesh <archana.ganesh@suse.com>
2025-05-15 11:00:43 -0700

    fix lint error

--- pkg/customflag/config.go
+++ pkg/customflag/config.go
@@ -1,6 +1,7 @@
 package customflag
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
@@ -192,7 +193,7 @@ type selinuxTestFlag bool
 
 func (s *selinuxTestFlag) Set(value string) error {
 	if value == "" {
-		return fmt.Errorf("invalid selinux test flag - cannot be empty")
+		return errors.New("invalid selinux test flag - cannot be empty")
 	}
 	// selinux test flag can only be true or false
 	// if value is not true or false, return an error
