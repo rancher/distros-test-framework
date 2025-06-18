adcc18bb8354d1b22a6f14ddedb235e219065d38
mdrahman-suse <md.rahman@suse.com>
2025-06-17 19:12:05 -0500

    Remove helm install from image to container (#253)

--- shared/aux.go
+++ shared/aux.go
@@ -175,6 +175,17 @@ func RunScp(c *Cluster, ip string, localPaths, remotePaths []string) error {
 	return nil
 }
 
+// InstallHelm installs helm on the container.
+func InstallHelm() (res string, err error) {
+	// Install Helm from local tarball
+	cmd := fmt.Sprintf("tar -zxvf %v/bin/helm-v3.18.3-linux-amd64.tar.gz -C /tmp && "+
+		"cp /tmp/linux-amd64/helm /usr/local/bin/helm && "+
+		"chmod +x /usr/local/bin/helm && "+
+		"helm version", BasePath())
+
+	return RunCommandHost(cmd)
+}
+
 // CheckHelmRepo checks a helm chart is available on the repo.
 func CheckHelmRepo(name, url, version string) (string, error) {
 	addRepo := fmt.Sprintf("helm repo add %s %s", name, url)
