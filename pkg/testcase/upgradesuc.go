779bd73b3c930cb8057fe7ad1480bab82be90475
Archana Ganesh <archana.ganesh@suse.com>
2025-07-11 09:48:33 -0700

    update suc to v0.15.2(suc.yaml)- includes role,clusterrole, etc. and crd apply in the workflow (#241)
    * update suc to latest yamls
    
    * update message
    
    * update message
    
    * adding arm yamls
    
    * apply url workload
    
    * update log line to info

--- pkg/testcase/upgradesuc.go
+++ pkg/testcase/upgradesuc.go
@@ -18,9 +18,7 @@ func TestUpgradeClusterSUC(cluster *shared.Cluster, k8sClient *k8s.Client, versi
 
 	shared.LogLevel("info", "Upgrading SUC to version: %s\n", version)
 
-	workloadErr := shared.ManageWorkload("apply", "suc.yaml")
-	Expect(workloadErr).NotTo(HaveOccurred(),
-		"system-upgrade-controller manifest did not deploy successfully")
+	applySucYamls()
 
 	getPodsSystemUpgrade := "kubectl get pods -n system-upgrade --kubeconfig="
 	err := assert.CheckComponentCmdHost(
@@ -51,12 +49,40 @@ func TestUpgradeClusterSUC(cluster *shared.Cluster, k8sClient *k8s.Client, versi
 		return fmt.Errorf("failed to write file: %s", err)
 	}
 
-	workloadErr = shared.ManageWorkload("apply", "plan.yaml")
-	Expect(workloadErr).NotTo(HaveOccurred(), "failed to upgrade cluster.")
+	planApplyErr := shared.ManageWorkload("apply", "plan.yaml")
+	Expect(planApplyErr).NotTo(HaveOccurred(), "failed to upgrade cluster - apply plan.yaml step failed.")
 
 	ok, err := k8sClient.CheckClusterHealth(0)
 	Expect(err).NotTo(HaveOccurred(), err, "error checking cluster health")
 	Expect(ok).To(BeTrue(), "cluster health check failed")
 
 	return nil
+}
+
+func applySucYamls() {
+	sucUrl := "https://github.com/rancher/system-upgrade-controller/releases/latest/download/system-upgrade-controller.yaml"
+	sucCRDUrl := "https://github.com/rancher/system-upgrade-controller/releases/latest/download/crd.yaml"
+
+	shared.LogLevel("info", "Applying system-upgrade-controller manifest from url: %s", sucUrl)
+	applyErr := shared.ApplyWorkloadURL(sucUrl)
+	if applyErr != nil {
+		shared.LogLevel(
+			"warn", "error applying system-upgrade-controller manifest from url: %s error: %v", sucUrl, applyErr)
+		shared.LogLevel("debug", "applying system-upgrade-controller manifest from local file")
+		// Fallback to local file if URL fails
+		applyErr = shared.ManageWorkload("apply", "suc.yaml")
+	}
+	Expect(applyErr).NotTo(HaveOccurred(),
+		"system-upgrade-controller manifest did not deploy successfully")
+
+	shared.LogLevel("debug", "Applying SUC CRD manifest from url: %s", sucCRDUrl)
+	applyErr = shared.ApplyWorkloadURL(sucCRDUrl)
+	if applyErr != nil {
+		shared.LogLevel("warn", "error applying SUC CRD manifest from url: %s error: %v", sucCRDUrl, applyErr)
+		shared.LogLevel("debug", "applying SUC CRD manifest from local file")
+		// Fallback to local file if URL fails
+		applyErr = shared.ManageWorkload("apply", "suc_crd.yaml")
+	}
+	Expect(applyErr).NotTo(HaveOccurred(),
+		"suc_crd.yaml apply did not deploy successfully")
 }
