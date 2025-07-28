78ad3dc4cf4e8cc18239b3758fd5ddcd3aebee98
Archana Ganesh <archana.ganesh@suse.com>
2025-07-28 14:55:58 -0700

    adding region (#262)

--- modules/rke2/worker/instances_worker.tf
+++ modules/rke2/worker/instances_worker.tf
@@ -48,7 +48,7 @@ resource "aws_instance" "worker" {
     ]
   }
   provisioner "local-exec" {
-    command = "echo \"${var.node_os}\" | grep -q \"slemicro\" && aws ec2 reboot-instances --instance-ids \"${self.id}\" && sleep 90 || exit 0"
+    command = "echo \"${var.node_os}\" | grep -q \"slemicro\" && aws ec2 reboot-instances --instance-ids \"${self.id}\" --region \"${var.region}\" && sleep 90 || exit 0"
   }
   provisioner "file" {
     source = "../install/join_rke2_agent.sh"
@@ -62,7 +62,7 @@ resource "aws_instance" "worker" {
     ]
   }
   provisioner "local-exec" {
-    command = "echo \"${var.node_os}\" | grep -q \"slemicro\" && aws ec2 reboot-instances --instance-ids \"${self.id}\" && sleep 90 || exit 0"
+    command = "echo \"${var.node_os}\" | grep -q \"slemicro\" && aws ec2 reboot-instances --instance-ids \"${self.id}\" --region \"${var.region}\" && sleep 90 || exit 0"
   }
   provisioner "remote-exec" {
     inline = [<<-EOT
