4fe10260a1ed91233037357549066c4e3aac8780
Francisco <francisco.moral@suse.com>
2025-11-24 14:30:54 -0300

    update release check script
    Signed-off-by: Francisco <francisco.moral@suse.com>

--- scripts/release_checks.sh
+++ scripts/release_checks.sh
@@ -220,13 +220,19 @@ verify_rke2_packaging () {
 }
 
 verify_prime_registry () {
+    SYS_AGENT_OUTFILE="sys_agent_${RANDOM_INT}"
+    UPGRADE_OUTFILE="upgrade_${RANDOM_INT}"
+    RKE2_RUNTIME_OUTFILE="rke2_runtime_${RANDOM_INT}"
+
     printf '\n==== VERIFY PRIME REGISTRY FOR Product: %s Version Prefix: %s Version Suffix: %s: ====\n' "${PRODUCT}" "${VERSION_PREFIX}" "${VERSION_SUFFIX}"
 
-    # rke2-runtime is only for rke2 product
     if [ "${PRODUCT}" = "rke2" ]; then
-        RKE2_RUNTIME_URL="docker://registry.rancher.com/rancher/rke2-runtime"
-        RKE2_RUNTIME_OUTFILE="rke2_runtime_${RANDOM_INT}"
-
+        if echo "${VERSION_PREFIX}" | grep -q "rc"; then
+            RKE2_RUNTIME_URL="docker://stgregistry.suse.com/rancher/rke2-runtime"
+        else
+            RKE2_RUNTIME_URL="docker://registry.rancher.com/rancher/rke2-runtime"
+        fi
+        
         if echo "${VERSION_PREFIX}" | grep -q "rc"; then
             debug_log "skopeo list-tags ${RKE2_RUNTIME_URL} | grep ${VERSION_PREFIX} | grep ${VERSION_SUFFIX} | tee -a ${RKE2_RUNTIME_OUTFILE}"
             skopeo list-tags "${RKE2_RUNTIME_URL}" | grep "${VERSION_PREFIX}" | grep "${VERSION_SUFFIX}" | tee -a "${RKE2_RUNTIME_OUTFILE}"
@@ -236,29 +242,35 @@ verify_prime_registry () {
         fi
     
         RKE2_RUNTIME_COUNT=$(wc -l < "${RKE2_RUNTIME_OUTFILE}")
-        verify_count "${RKE2_RUNTIME_COUNT}" "4" "RKE2 Runtime (in prime registry)"
-        rm -rf "${RKE2_RUNTIME_OUTFILE}"
+        verify_count "${RKE2_RUNTIME_COUNT}" "1" "RKE2 Runtime(in prime registry)"
     fi
 
-    # Verify system-agent-installer and upgrade images in prime registry for k3s and rke2 products
-    URL_ITEMS="system-agent-installer-${PRODUCT} ${PRODUCT}-upgrade"
+    if echo "${VERSION_PREFIX}" | grep -q "rc"; then
+        SYS_AGENT_INSTALLER_URL="docker://stgregistry.suse.com/rancher/system-agent-installer-${PRODUCT}"
+    else
+        SYS_AGENT_INSTALLER_URL="docker://registry.rancher.com/rancher/system-agent-installer-${PRODUCT}"
+    fi
+    debug_log "skopeo list-tags ${SYS_AGENT_INSTALLER_URL} | grep ${VERSION_PREFIX} | grep ${VERSION_SUFFIX} | tee -a ${SYS_AGENT_OUTFILE}"
+    skopeo list-tags "${SYS_AGENT_INSTALLER_URL}" | grep "${VERSION_PREFIX}" | grep "${VERSION_SUFFIX}" | tee -a "${SYS_AGENT_OUTFILE}"
+    
+    SYS_AGENT_COUNT=$(wc -l < "${SYS_AGENT_OUTFILE}")
+    verify_count "${SYS_AGENT_COUNT}" "1" "System Agent Installer for ${PRODUCT} (in prime registry)"
 
-    for ITEM in $URL_ITEMS; do
-        PRIME_URL="docker://registry.rancher.com/rancher/${ITEM}"
-        OUTFILE="${ITEM}_${VERSION_PREFIX}_${RANDOM_INT}"
+    if echo "${VERSION_PREFIX}" | grep -q "rc"; then
+        UPGRADE_INSTALLER_URL="docker://stgregistry.suse.com/rancher/${PRODUCT}-upgrade"
+    else    
+        UPGRADE_INSTALLER_URL="docker://registry.rancher.com/rancher/${PRODUCT}-upgrade"
+    fi
+    debug_log "skopeo list-tags ${UPGRADE_INSTALLER_URL} | grep ${VERSION_PREFIX} | grep ${VERSION_SUFFIX} | tee -a ${UPGRADE_OUTFILE}"
+    skopeo list-tags "${UPGRADE_INSTALLER_URL}" | grep "${VERSION_PREFIX}" | grep "${VERSION_SUFFIX}" | tee -a "${UPGRADE_OUTFILE}"
+    
+    UPG_COUNT=$(wc -l < "${UPGRADE_OUTFILE}")
+    verify_count "${UPG_COUNT}" "1" "${PRODUCT}-upgrade (in prime registry)"
 
-        debug_log "skopeo list-tags ${PRIME_URL} | grep ${VERSION_PREFIX} | grep ${VERSION_SUFFIX} | tee -a ${OUTFILE}"
-        skopeo list-tags "${PRIME_URL}" | grep "${VERSION_PREFIX}" | grep "${VERSION_SUFFIX}" | tee -a "${OUTFILE}"
-        
-        COUNT=$(wc -l < "${OUTFILE}")
-        if echo "${VERSION_PREFIX}" | grep -q "rc"; then
-            verify_count "${COUNT}" "0" "${ITEM} (in prime registry)"
-        else
-            verify_count "${COUNT}" "1" "${ITEM} (in prime registry)"
-        fi
 
-        rm -rf "${OUTFILE}"
-    done
+    rm -rf "${UPGRADE_OUTFILE}"
+    rm -rf "${SYS_AGENT_OUTFILE}"
+    rm -rf "${RKE2_RUNTIME_OUTFILE}"
 }
 
 verify_lts () {
@@ -310,7 +322,7 @@ do
     printf "===================== DONE ==========================\n"
 done
 
-if [ -f "${FAILURE_FILE}" ]; then
+if [ -f $FAILURE_FILE ]; then
     printf "==========================================================================
                         FAILURE SUMMARY 
 ==========================================================================\n"
