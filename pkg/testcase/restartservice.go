6504b734758378efe58c704a121528d743cad89f
mdrahman-suse <md.rahman@suse.com>
2026-01-30 16:32:01 -0600

    add role_order in tf, add delay in restart service, add logging for airgap install

--- pkg/testcase/restartservice.go
+++ pkg/testcase/restartservice.go
@@ -9,9 +9,10 @@ import (
 func TestRestartService(cluster *shared.Cluster) {
 	ms := shared.NewManageService(1, 1)
 	serverAction := shared.ServiceAction{
-		Service:  cluster.Config.Product,
-		Action:   "restart",
-		NodeType: "server",
+		Service:       cluster.Config.Product,
+		Action:        "restart",
+		NodeType:      "server",
+		ExplicitDelay: 15,
 	}
 	for _, ip := range cluster.ServerIPs {
 		_, err := ms.ManageService(ip, []shared.ServiceAction{serverAction})
@@ -20,9 +21,10 @@ func TestRestartService(cluster *shared.Cluster) {
 
 	if cluster.NumAgents > 0 {
 		agentAction := shared.ServiceAction{
-			Service:  cluster.Config.Product,
-			Action:   "restart",
-			NodeType: "agent",
+			Service:       cluster.Config.Product,
+			Action:        "restart",
+			NodeType:      "agent",
+			ExplicitDelay: 5,
 		}
 		for _, ip := range cluster.AgentIPs {
 			_, err := ms.ManageService(ip, []shared.ServiceAction{agentAction})
