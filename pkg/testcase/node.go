42279cb5e5d65ab2ba35f62d4aabb6ae3f16398d
ERYN Dawkins <eryn.dawkins@suse.com>
2026-05-12 14:50:22 -0500

    golangci-lint fixes

--- pkg/testcase/node.go
+++ pkg/testcase/node.go
@@ -170,7 +170,8 @@ func TestNodeCPUUsageBelowThreshold(maxCPUPercent int, applyWorkload, deleteWork
 
 		overThreshold, checkErr := checkNodeCPUThreshold(maxCPUPercent, res)
 		g.Expect(checkErr).To(BeNil())
-		g.Expect(overThreshold).To(BeEmpty(), "Found nodes above %d%% CPU utilization: %v\nFull output:\n%s", maxCPUPercent, overThreshold, res)
+		g.Expect(overThreshold).To(BeEmpty(), "Found nodes above %d%% CPU utilization: %v\nFull output:\n%s",
+		 maxCPUPercent, overThreshold, res)
 
 		return true
 	}, timeout, "10s").Should(BeTrue(), "CPU usage on one or more nodes exceeded %d%% threshold", maxCPUPercent)
